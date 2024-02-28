5bf4ec94c478783ace2c445ab7455d29b29d3f3f
khokh <khoiskyi.oleksandr@chnu.edu.ua>
2024-02-29 00:28:32 +0200

    done

---
+++ Lab6CSharp/Animal.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Lab6CSharp
+{
+    class Animal : UseInterface
+    {
+        public string Name { get; set; }
+
+        public void DisplayUserInfo()
+        {
+            Console.WriteLine($"I am an animal. My name is {Name}");
+        }
+    }
+}
